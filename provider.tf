82cb3e6ce60e5e5becbf18c33c2e07fbb716517f
soyeon5-5 <ems5-5@naver.com>
2023-04-20 17:36:48 +0900

    backend-0

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "s3" {
     bucket = "cute-sy-s3" #S3 버킷명
-    key    = "terraform.tfstate" #terraform.tfstate 파일위치 ex) aws-an2/jenkins_terraform_test/terraform.tfstate
+    key    = "1" #terraform.tfstate 파일위치 ex) aws-an2/jenkins_terraform_test/terraform.tfstate
     region = "ap-south-1" # 리전
     dynamodb_table = "cute-sy-db" #테라폼 백엔드 구성용 DynamoDB 테이블명
     encrypt = true
