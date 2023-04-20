17988a920e9ae39c55579940a49e8b992e3761d8
soyeon5-5 <ems5-5@naver.com>
2023-04-20 18:30:27 +0900

    plan01

--- provider.tf
+++ provider.tf
@@ -1,13 +1,13 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source  = "hashicorp/terraform-provider-aws"
       version = "~> 4.0"
     }
   }
   backend "s3" {
     bucket = "cute-sy-s3" #S3 버킷명
-    key    = "1" #terraform.tfstate 파일위치 ex) aws-an2/jenkins_terraform_test/terraform.tfstate
+    key    = "terraform.tfstate" #terraform.tfstate 파일위치 ex) aws-an2/jenkins_terraform_test/terraform.tfstate
     region = "ap-south-1" # 리전
     dynamodb_table = "api-nonprd-statelock" #테라폼 백엔드 구성용 DynamoDB 테이블명
     encrypt = true
