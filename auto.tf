f73582cd5dbed492c10631b4f667f671056dd5b3
soyeon5-5 <ems5-5@naver.com>
2023-04-21 18:02:33 +0900

    alb.tf, auto.tf apply again

--- auto.tf
+++ auto.tf
@@ -7,11 +7,11 @@ resource "aws_launch_template" "web-launch-template" {
   instance_type   = "t3.2xlarge"
   key_name        = "genkins-key"
 
-user_data = base64encode(<<EOF2
+user_data = <<-EOF2
 #!/bin/bash
 sudo mount -t efs -o tls fs-09e4715f324aef5d6:/ /var/www/html
 EOF
-)
+
   # Required when using a launch configuration with an auto scaling group.
   lifecycle {
     create_before_destroy = true
@@ -36,10 +36,3 @@ resource "aws_autoscaling_group" "web-asg2" {
     propagate_at_launch = true
   }
 }
-
-
-
-
-
-
- 
