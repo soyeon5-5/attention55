1a5b49e3bab2ea9fae83ad8017ab3713facb4a35
soyeon5-5 <ems5-5@naver.com>
2023-04-24 11:21:15 +0900

    newaws

--- provider.tf
+++ provider.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "attention-s3" #S3 버킷명
+    bucket = "jen-s3" #S3 버킷명
     key    = "terraform.tfstate" #terraform.tfstate 파일위치 ex) aws-an2/jenkins_terraform_test/terraform.tfstate
     region = "ap-northeast-2" # 리전
     dynamodb_table = "dyDB-jenkins" #테라폼 백엔드 구성용 DynamoDB 테이블명
